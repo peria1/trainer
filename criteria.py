7d14bcbb4f3e498479009d3136c25014c3e20aea
peria1 <37117878+peria1@users.noreply.github.com>
2020-01-10 09:58:06 -0800

    Fixed an error in target_residual_correlation.

--- criteria.py
+++ criteria.py
@@ -25,13 +25,25 @@ def decorrelate_target_residuals(yhat,y):
 #tht = 0.5 * (1i/2)*(log(1-1i*z) - log(1+1i*z));
     
 def target_residual_correlation(yhat,y):
-        
+#    import numpy as np
+    
     res = yhat - y;
-    dx = res - torch.mean(res,1,keepdim=True)
-    dy = y - torch.mean(y,1,keepdim=True)
-    sx = torch.sqrt(torch.mean(torch.pow(dx,2),1,keepdim=True))
-    sy = torch.sqrt(torch.mean(torch.pow(dy,2),1,keepdim=True))
-    return torch.mean(torch.abs(torch.mean(dx*dy,1,keepdim=True)/(sx*sy)))
+    x0 = torch.mean(res,0,keepdim=True)
+    y0 = torch.mean(y,  0,keepdim=True)
+    dx = res - x0
+    dy = y - y0
+    
+#    print('np corr',np.corrcoef(dx.cpu().detach().numpy(),\
+#                      dy.cpu().detach().numpy()),0)
+#    
+#    print(dx.size(), dy.size())
+    sx = torch.sqrt(torch.mean(torch.pow(dx,2),0,keepdim=True))
+    sy = torch.sqrt(torch.mean(torch.pow(dy,2),0,keepdim=True))
+    
+    corrs = torch.mean(dx*dy,0,keepdim=True)/(sx*sy)
+#    print('individual corrs',corrs)
+
+    return torch.mean(torch.abs(corrs))
 
 def mse_plus_corr(yhat,y):
     mse = nn.MSELoss()
