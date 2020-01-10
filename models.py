8da80e0852601fce93f3e74a4d5afe57c3696dcf
peria1 <37117878+peria1@users.noreply.github.com>
2020-01-09 16:32:02 -0800

    In bisect_to_power_of_two, make sure nout is handled correctly. in VGGNet, run final classifier through softmax.

--- models.py
+++ models.py
@@ -67,6 +67,7 @@ class bisect_to_power_of_two(nn.Module):
         
         self.npts = npts
         self.nbatch = nbatch
+        self.nout = nout
         
         n = npts*2
         self.layer_list = nn.ModuleList([nn.Linear(npts,n)])
@@ -438,7 +439,7 @@ class VGGNet(VGG):
         
         score = self.classifier(torch.flatten(self.avgpool(output['x5']),1))
 
-        return torch.sigmoid(score)
+        return torch.softmax(score,1)
 
 
 
