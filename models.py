9107be10381cd1c66d0b7a2c3537cec7594955fa
peria1 <37117878+peria1@users.noreply.github.com>
2020-04-17 10:04:45 -0700

    Moved local_prep_display from problems to models. Made YOLAB specific imports occur inside YOLAB __init__. Successfully called local_evalimage from the iPython prompt and saw an image with bboxes and labels. Not sure about masks.

--- models.py
+++ models.py
@@ -20,19 +20,7 @@ from torch import nn
 from torchvision.models.vgg import VGG
 import numpy as np
 
-#from criteria import *
-#from ..yolact.data import *
-#from .. import *
-#from ..yolact.yolact import *
-import sys
-if not 'yolact' in sys.path[1]:
-    sys.path.insert(1, '../yolact/')
-    
-import yolact
-from utils.augmentations import SSDAugmentation #, FastBaseTransform, BaseTransform
-from yolact import Yolact
-from train import  NetWithLoss, CustomDataParallel, MultiBoxLoss, prepare_data
-import data as D  
+from criteria import *
 
 
 
@@ -306,7 +294,7 @@ class vectorVAE(nn.Module):   # moved to vectorVAE
         inp, target = problem.get_input_and_target()
         assert(len(inp.size())==2)
         nbatch, npts = inp.size()
-        nout = target.size()[1]
+#        nout = target.size()[1]
 
         self.npts = npts
         self.nbatch = nbatch        
@@ -468,15 +456,41 @@ class YOLAB(nn.Module):
     def __init__(self, problem):
         super().__init__()
         
+        from utils.augmentations import FastBaseTransform
+        self.FastBaseTransform = FastBaseTransform
+        
+        import cv2
+        self.cv2 = cv2
+        
+        import matplotlib.pyplot as plt
+        self.plt = plt
+        
+        from layers.output_utils import postprocess, undo_image_transformation
+        self.postprocess = postprocess
+        self.undo_image_transformation = undo_image_transformation
+        
+        from utils import timer
+        self.timer = timer
+
+        import sys
+        syspathsave = None
+        if not 'yolact' in sys.path[1]:
+            import copy
+            syspathsave = copy.copy(sys.path)
+            sys.path.insert(1, '../yolact/')
+            
+        from yolact import Yolact
+        from train import MultiBoxLoss
+        import data as D  
+        self.D = D
+        
+        from collections import defaultdict
+        self.color_cache = defaultdict(lambda: {})
+
+        
         net = Yolact()
         net.train()
         net.init_weights(backbone_path='../yolact/weights/' + D.cfg.backbone.path)
-
-
-#        print('num_classes is',D.cfg.num_classes)
-#        print('pos_threshold=',D.cfg.positive_iou_threshold)
-#        print('neg_threshold=',D.cfg.negative_iou_threshold)
-#        print('negpos_ratio=', D.cfg.ohem_negpos_ratio)
 
         criterion = MultiBoxLoss(num_classes=D.cfg.num_classes,
                                  pos_threshold=D.cfg.positive_iou_threshold,
@@ -486,6 +500,9 @@ class YOLAB(nn.Module):
         self.net = net
         self.criterion = criterion
 
+        if syspathsave:
+            sys.path = syspathsave
+
     def forward(self, images): 
         predsT = self.net(images)
         return predsT
@@ -493,15 +510,176 @@ class YOLAB(nn.Module):
     def custom_loss(self, predsT, target):        
         targets, masks, num_crowds = target
         self.net.train()
-#        print('custom loss, images has type',type(images))
-#        if type(images) is dict:
-#            print(images.keys())
-            
-#        predsT = self.net(images)
         losses = self.criterion(self.net, predsT, targets[0], masks[0], num_crowds[0])
         loss = sum([losses[k] for k in losses])
-        
         return loss
+
+    def local_evalimage(self, net, path:str):
+        frame = torch.from_numpy(self.cv2.imread(path)).cuda().float()
+        print('frame size is', frame.size())
+        batch = self.FastBaseTransform()(frame.unsqueeze(0))
+        print('Batch size is',batch.size())
+        net.eval()
+        with torch.no_grad():
+            preds = net(batch)
+        net.train()
+    
+        img_numpy = self.local_prep_display(preds, frame, None, None, undo_transform=False)
+        
+        img_numpy = img_numpy[:, :, (2, 1, 0)]
+    
+        self.plt.imshow(img_numpy)
+        self.plt.title(path)
+        self.plt.show()
+    
+
+
+    def local_prep_display(self,dets_out, img, h, w, undo_transform=True, class_color=False, mask_alpha=0.45, fps_str=''):
+        """
+        Note: If undo_transform=False then im_h and im_w are allowed to be None.
+        
+        I don't have args available, so I need to create it and fill it with the defaults. 
+        
+        
+        """
+    #    print('local_prep_display, type(dets_out) is',type(dets_out))
+        
+        D = self.D  #  D contains everything from config.py
+        color_cache = self.color_cache
+        
+        top_k = 5    # May want to adjust this. If there are 6 objects in view, what happens? 
+        score_threshold = 0.0
+        display_masks = True
+        display_text = True
+        display_bboxes = True
+        display_scores = True
+        display_fps = False
+    
+        if undo_transform:
+            img_numpy = self.undo_image_transformation(img, w, h)
+            img_gpu = torch.Tensor(img_numpy).cuda()
+        else:
+            img_gpu = img / 255.0
+            h, w, _ = img.shape
+        
+        with self.timer.env('Postprocess'):
+            save =D.cfg.rescore_bbox
+            D.cfg.rescore_bbox = True
+            t = self.postprocess(dets_out, w, h, visualize_lincomb = False,
+                                            crop_masks        = True,
+                                            score_threshold   = score_threshold)
+    
+            D.cfg.rescore_bbox = save
+    
+        with self.timer.env('Copy'):
+            idx = t[1].argsort(0, descending=True)[:top_k]
+            
+            if D.cfg.eval_mask_branch:
+                # Masks are drawn on the GPU, so don't copy
+                masks = t[3][idx]
+            classes, scores, boxes = [x[idx].cpu().detach().numpy() for x in t[:3]]
+    
+        num_dets_to_consider = min(top_k, classes.shape[0])
+        for j in range(num_dets_to_consider):
+            if scores[j] < score_threshold:
+                num_dets_to_consider = j
+                break
+    
+        # Quick and dirty lambda for selecting the color for a particular index
+        # Also keeps track of a per-gpu color cache for maximum speed
+        def get_color(j, on_gpu=None):
+#            global color_cache
+            color_idx = (classes[j] * 5 if class_color else j * 5) % len(D.COLORS)
+            
+            if on_gpu is not None and color_idx in color_cache[on_gpu]:
+                return color_cache[on_gpu][color_idx]
+            else:
+                color = D.COLORS[color_idx]
+                if not undo_transform:
+                    # The image might come in as RGB or BRG, depending
+                    color = (color[2], color[1], color[0])
+                if on_gpu is not None:
+                    color = torch.Tensor(color).to(on_gpu).float() / 255.
+                    color_cache[on_gpu][color_idx] = color
+                return color
+    
+        # First, draw the masks on the GPU where we can do it really fast
+        # Beware: very fast but possibly unintelligible mask-drawing code ahead
+        # I wish I had access to OpenGL or Vulkan but alas, I guess Pytorch tensor operations will have to suffice
+        if display_masks and D.cfg.eval_mask_branch and num_dets_to_consider > 0:
+            # After this, mask is of size [num_dets, h, w, 1]
+            masks = masks[:num_dets_to_consider, :, :, None]
+            
+            # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
+            colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
+            masks_color = masks.repeat(1, 1, 1, 3) * colors * mask_alpha
+    
+            # This is 1 everywhere except for 1-mask_alpha where the mask is
+            inv_alph_masks = masks * (-mask_alpha) + 1
+            
+            # I did the math for this on pen and paper. This whole block should be equivalent to:
+            #    for j in range(num_dets_to_consider):
+            #        img_gpu = img_gpu * inv_alph_masks[j] + masks_color[j]
+            masks_color_summand = masks_color[0]
+            if num_dets_to_consider > 1:
+                inv_alph_cumul = inv_alph_masks[:(num_dets_to_consider-1)].cumprod(dim=0)
+                masks_color_cumul = masks_color[1:] * inv_alph_cumul
+                masks_color_summand += masks_color_cumul.sum(dim=0)
+    
+            img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+        
+        if display_fps:
+                # Draw the box for the fps on the GPU
+            font_face = self.cv2.FONT_HERSHEY_DUPLEX
+            font_scale = 0.6
+            font_thickness = 1
+    
+            text_w, text_h = self.cv2.getTextSize(fps_str, font_face, font_scale, font_thickness)[0]
+    
+            img_gpu[0:text_h+8, 0:text_w+8] *= 0.6 # 1 - Box alpha
+    
+    
+        # Then draw the stuff that needs to be done on the cpu
+        # Note, make sure this is a uint8 tensor or opencv will not anti alias text for whatever reason
+        img_numpy = (img_gpu * 255).byte().cpu().numpy()
+    
+        if display_fps:
+            # Draw the text on the CPU
+            text_pt = (4, text_h + 2)
+            text_color = [255, 255, 255]
+    
+            self.cv2.putText(img_numpy, fps_str, text_pt, font_face, font_scale, text_color, font_thickness, self.cv2.LINE_AA)
+        
+        if num_dets_to_consider == 0:
+            return img_numpy
+    
+        if display_text or display_bboxes:
+            for j in reversed(range(num_dets_to_consider)):
+                x1, y1, x2, y2 = boxes[j, :]
+                color = get_color(j)
+                score = scores[j]
+    
+                if display_bboxes:
+                    self.cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
+    
+                if display_text:
+                    _class = D.cfg.dataset.class_names[classes[j]]
+                    text_str = '%s: %.2f' % (_class, score) if display_scores else _class
+    
+                    font_face = self.cv2.FONT_HERSHEY_DUPLEX
+                    font_scale = 0.6
+                    font_thickness = 1
+    
+                    text_w, text_h = self.cv2.getTextSize(text_str, font_face, font_scale, font_thickness)[0]
+    
+                    text_pt = (x1, y1 - 3)
+                    text_color = [255, 255, 255]
+    
+                    self.cv2.rectangle(img_numpy, (x1, y1), (x1 + text_w, y1 - text_h - 4), color, -1)
+                    self.cv2.putText(img_numpy, text_str, text_pt, font_face, font_scale, text_color, font_thickness, self.cv2.LINE_AA)
+                
+        
+        return img_numpy
 
 
 
