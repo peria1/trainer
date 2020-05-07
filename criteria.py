47e9cf63ae52628fc970b93c107caff462a2fa05
William Peria <peria1@comcast.net>
2020-05-07 08:29:05 -0700

    THis time, additional unmodified files did NOT show up as modified and staged for commit.

--- criteria.py
+++ criteria.py
@@ -52,6 +52,48 @@ def target_residual_slope(yhat,y):
     
     return rho*dres/dy
 
+def target_residual_correlation2(yhat,y):
+#    import numpy as np
+    
+    res = yhat - y;
+    x0 = torch.mean(res,1,keepdim=True)
+    y0 = torch.mean(y,  1,keepdim=True)
+    dx = res - x0
+    dy = y - y0
+    
+#    print('np corr',np.corrcoef(dx.cpu().detach().numpy(),\
+#                      dy.cpu().detach().numpy()),0)
+#    
+#    print(dx.size(), dy.size())
+    sx = torch.sqrt(torch.mean(torch.pow(dx,2),1,keepdim=True))
+    sy = torch.sqrt(torch.mean(torch.pow(dy,2),1,keepdim=True))
+    
+    corrs = torch.mean(dx*dy,1,keepdim=True)/(sx*sy)
+#    print('individual corrs',corrs)
+
+    return torch.mean(torch.abs(corrs))
+
+def target_residual_correlation3(yhat,y):
+#    import numpy as np
+    
+    res = yhat - y;
+    x0 = torch.mean(res,1,keepdim=True)
+    y0 = torch.mean(y,  1,keepdim=True)
+    dx = res - x0
+    dy = y - y0
+    
+#    print('np corr',np.corrcoef(dx.cpu().detach().numpy(),\
+#                      dy.cpu().detach().numpy()),0)
+#    
+#    print(dx.size(), dy.size())
+    sx = torch.sqrt(torch.mean(torch.pow(dx,2),1,keepdim=True))
+    sy = torch.sqrt(torch.mean(torch.pow(dy,2),1,keepdim=True))
+    
+    corrs = torch.mean(dx*dy,1,keepdim=True)/(sx*sy)
+#    print('individual corrs',corrs)
+
+    return torch.max(torch.abs(corrs))
+
 def mse_plus_corr(yhat,y):
     mse = nn.MSELoss()
     return mse(yhat,y) + torch.mean(target_residual_correlation(yhat,y))
@@ -72,6 +114,7 @@ def diff_and_product(yhat,y):
     L1 = nn.L1Loss()
     return (1+torch.mean(target_residual_correlation(yhat,y)))*L1(yhat,y)
 
+
 def diff_and_slope(yhat,y):
     L1 = nn.L1Loss()
     return (1+torch.sum(target_residual_slope(yhat,y)))*L1(yhat,y)
@@ -83,6 +126,15 @@ def max_abs(yhat,y):
 def diff_and_max(yhat,y):
     L1 = nn.L1Loss()
     return L1(yhat,y) + max_abs(yhat, y)
+
+def diff_and_product2(yhat,y):
+    L1 = nn.L1Loss()
+    return (1+target_residual_correlation2(yhat,y))*L1(yhat,y)
+
+def diff_and_product3(yhat,y):
+    L1 = nn.L1Loss()
+    return (1+target_residual_correlation3(yhat,y))*L1(yhat,y)
+
 
 def diff_max_slope(yhat,y):
     return diff_and_max(yhat,y) + torch.sum(target_residual_slope(yhat,y))
